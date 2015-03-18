d2e33347c75327e9a50b99b5cf541d5f2066ff6a
Le Tuan Anh <tuananh.ke@gmail.com>
2015-03-18 14:21:39 +0800

    Add helloworld demo

---
+++ basic/helloworld.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Hash starts a comment. Python will ignore anything written here.
+# The first line of the application helps the OS to figure out how to run the application (i.e. using Python)
+# The second line specifies the encoding used in this file.
+# If you still have no idea, just copy those two to your file :P
+
+#------------------------------------------------------------------------------
+# Define the main method
+#------------------------------------------------------------------------------
+def main():
+	'''The main entry of the application (i.e. The tasks should start from here)
+	'''
+	print("Hello World") # Print the text `Hello World` to the screen
+	pass # Do nothing, yes Python has a statement to do nothing :D
+
+#------------------------------------------------------------------------------
+# Check if this file is run as an application
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+	# If the condition is true, execute the main method
+	main()
